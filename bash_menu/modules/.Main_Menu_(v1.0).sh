dc4170a18c9927322d20e293456c3ef294fd796b
rfencl <rick_fencl@hotmail.com>
2024-04-20 23:20:37 -0700

    Added the ability to have more than 10 items on the menu.
    index the menu through an array of letters of the alphabet.

--- bash_menu/modules/.Main_Menu_(v1.0).sh
+++ bash_menu/modules/.Main_Menu_(v1.0).sh
@@ -3,16 +3,36 @@ do
 let itemCnt=0
 clear
 cntrPrompt "$title"
-cntr ; echo -e  "${RED}${itemCnt}: Exit";   ((itemCnt++))
-cntr ; echo -e "${LTCYAN}${itemCnt}:" Launch Midnight Commander; ((itemCnt++))
-cntr ; echo -e "${LTCYAN}${itemCnt}:" Launch HTOP; ((itemCnt++))
+cntr ; echo -e  "${RED}${indexes[itemCnt]}: Exit";   ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" Launch Midnight Commander; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" Launch HTOP; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" Launch Archive Manager; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" Launch meld; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" fn5; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" fn6; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" fn7; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" fn8; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" fn9; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" fn10; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" fn11; ((itemCnt++))
+cntr ; echo -e "${LTCYAN}${indexes[itemCnt]}:" fn12; ((itemCnt++))
 read -sn1
 let labelCnt=0
 let curDwn=$((itemCnt+1))
 case "$REPLY" in
-$((labelCnt++))) clear; exit 0;;
-$((labelCnt++))) (mc);;
-$((labelCnt++))) (htop);;
+${indexes[$((labelCnt++))]})  clear; exit 0;;
+${indexes[$((labelCnt++))]})  (mc);;
+${indexes[$((labelCnt++))]})  (htop);;
+${indexes[$((labelCnt++))]})  (file-roller);;
+${indexes[$((labelCnt++))]})  (meld);;
+${indexes[$((labelCnt++))]})  (fn5);curDwn=1;;
+${indexes[$((labelCnt++))]})  (fn6);curDwn=1;;
+${indexes[$((labelCnt++))]})  (fn7);curDwn=1;;
+${indexes[$((labelCnt++))]})  (fn8);curDwn=1;;
+${indexes[$((labelCnt++))]})  (fn9);curDwn=1;;
+${indexes[$((labelCnt++))]})  (fn10);curDwn=1;;
+${indexes[$((labelCnt++))]})  (fn11);curDwn=1;;
+${indexes[$((labelCnt++))]})  (fn12);curDwn=1;;
  *) curDwn=1 ;;
 esac
 moveCursorDown $curDwn ; cntr ; getKey $curDwn
--- bash_menu/snippets/sharedVariables.sh
+++ bash_menu/snippets/sharedVariables.sh
@@ -18,7 +18,8 @@ if [ '-' != ${program::1} ]; then
   scriptBasename="$(basename ${scriptName} .sh)"
 fi
 
-
+# menu index conversions to allow more than nine
+indexes=(0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 # TIMESTAMPS
 # ------------------------------------------------------
 # Prints the current date and time in a variety of formats:
